1dcb7da37056f6ab5834ce08b4e69bc3fd9b0208
dataforestLTD <76258792+dataforestLTD@users.noreply.github.com>
2021-12-11 18:28:41 -0700

    working on token swap

--- index.js
+++ index.js
@@ -38,11 +38,13 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 const moment = require('moment-timezone');
 const numeral = require('numeral');
 const _ = require('lodash');
+const console = require('console');
 
 // SERVER CONFIG
 const PORT = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app).listen(PORT, () => console.log(`Listening on ${PORT}`));
+const POLLING_INTERVAL = 1000; // 1 Second = 1000
 
 // WEB3 CONFIG
 const web3 = new Web3(new HDWalletProvider(process.env.PRIVATE_KEY, process.env.RPC_URL));
@@ -59,27 +61,34 @@ const ACCOUNT = process.env.ACCOUNT;
 const HODL_ACCOUNT = process.env.HODL_ACCOUNT;
 
 // LOAD COIN ADDRESSES FROM JSON, MAKE LISTS
-const tokenList = require('./watchTokens.json');
-const priceChanges = {};
-const currentPriceTrend = {};
-const currentPriceList = {};
+const STABLE_COINS = require('./stableTokens.json');
+const SHIT_COINS = require('./shitTokens.json');
+const WATCH_COINS = require('./quickswapTokens.json');
+const ERC20_ABI = require("./erc20ABI.json");
+const PRICE_CHANGES = {};
+const CURRENT_PRICE_TREND = {};
+const CURRENT_PRICE_LIST = {};
+const SLIPPAGE = 0.97;
 const topFive = {};
 const currentHodl = {};
 
 // SET QUICKSWAP CONTRACT
-const qsABI = require('./qsABI.json');
-const console = require('console');
-const qsAddress = "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff"
-const qsSwapContract = new web3.eth.Contract(qsABI, qsAddress);
+const QUICKSWAP_ABI = require('./quickswapABI.json');
+const QUICKSWAP_ADDRESS = "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff"
+const QUICKSWAP_CONTRACT = new web3.eth.Contract(QUICKSWAP_ABI, QUICKSWAP_ADDRESS);
 
 // POPULATE PRICE CHANGE DICT
-for (var i = 0; i < tokenList.length; i++) {
-  priceChanges[tokenList[i].tokenAddress] = [];
+for (var i = 0; i < WATCH_COINS.length; i++) {
+  PRICE_CHANGES[WATCH_COINS[i].address] = [];
 }
 
 let priceMonitor;
 let monitoringPrice = false;
 let executingTrade = false;
+
+
+
+
 
 async function monitorPrice() {
   if (monitoringPrice) {
@@ -88,162 +97,165 @@ async function monitorPrice() {
   monitoringPrice = true;
 
   // LOOP THROUGH CONTRACT LIST
-  for (var i = 0; i < tokenList.length; i++) {
-    var obj = tokenList[i];
+  for (var i = 0; i < WATCH_COINS.length; i++) {
+    var obj = WATCH_COINS[i];
     var options = {
-      address: obj.tokenAddress,
+      address: obj.address,
       chain: "polygon",
       exchange: "quickswap"
     }
     // GET TOKEN PRICE
     try {
       var tokenPrice = await Moralis.Web3API.token.getTokenPrice(options);
-      console.log(obj.ticker, "has a value of $", tokenPrice.usdPrice.toFixed(18));
-      var changes = [priceChanges[obj.tokenAddress]];
+      console.log(obj.name, "has a value of $", tokenPrice.usdPrice.toFixed(18));
+      var changes = [PRICE_CHANGES[obj.address]];
       changes[0].push(tokenPrice.usdPrice);
-      priceChanges[obj.tokenAddress] = changes[0];
-      currentPriceList[obj.tokenAddress] = tokenPrice.usdPrice.toFixed(18);
+      PRICE_CHANGES[obj.address] = changes[0];
+      CURRENT_PRICE_LIST[obj.address] = tokenPrice.usdPrice.toFixed(18);
     }
     catch (error) {
       if (error.code != 141) {
         console.log(error);
-        console.log(obj.ticker);
+        console.log(obj.name);
       }
       else {
-        console.log("No Price Found for", obj.ticker);
+        console.log("No Price Found for", obj.name);
       }
     }
     // CHECK PRICE CHANGES
-    if (priceChanges[obj.tokenAddress].length >= 2) {
+    if (PRICE_CHANGES[obj.address].length >= 2) {
       var tokenChange = 0;
-      if (priceChanges[obj.tokenAddress][0] < priceChanges[obj.tokenAddress][1]) {
-        console.log(obj.ticker, "price went up %", ((priceChanges[obj.tokenAddress][1] - priceChanges[obj.tokenAddress][0]) / priceChanges[obj.tokenAddress][0]) * 100);
-        tokenChange = (((priceChanges[obj.tokenAddress][1] - priceChanges[obj.tokenAddress][0]) / priceChanges[obj.tokenAddress][0]) * 100);
+      if (PRICE_CHANGES[obj.address][0] < PRICE_CHANGES[obj.address][1]) {
+        console.log(obj.name, "price went up %", ((PRICE_CHANGES[obj.address][1] - PRICE_CHANGES[obj.address][0]) / PRICE_CHANGES[obj.address][0]) * 100);
+        tokenChange = (((PRICE_CHANGES[obj.address][1] - PRICE_CHANGES[obj.address][0]) / PRICE_CHANGES[obj.address][0]) * 100);
       }
-      if (priceChanges[obj.tokenAddress][0] > priceChanges[obj.tokenAddress][1]) {
-        console.log(obj.ticker, "price went down %", ((priceChanges[obj.tokenAddress][0] - priceChanges[obj.tokenAddress][1]) / priceChanges[obj.tokenAddress][0]) * 100);
-        tokenChange = (((priceChanges[obj.tokenAddress][0] - priceChanges[obj.tokenAddress][1]) / priceChanges[obj.tokenAddress][0]) * -100);
+      if (PRICE_CHANGES[obj.address][0] > PRICE_CHANGES[obj.address][1]) {
+        console.log(obj.name, "price went down %", ((PRICE_CHANGES[obj.address][0] - PRICE_CHANGES[obj.address][1]) / PRICE_CHANGES[obj.address][0]) * 100);
+        tokenChange = (((PRICE_CHANGES[obj.address][0] - PRICE_CHANGES[obj.address][1]) / PRICE_CHANGES[obj.address][0]) * -100);
       }
-      if (priceChanges[obj.tokenAddress][0] == priceChanges[obj.tokenAddress][1]) {
-        console.log(obj.ticker, "price didn't change");
+      if (PRICE_CHANGES[obj.address][0] == PRICE_CHANGES[obj.address][1]) {
+        console.log(obj.name, "price didn't change");
         tokenChange = 0;
       }
-      priceChanges[obj.tokenAddress].pop[0];
-      currentPriceTrend[obj.tokenAddress] = tokenChange;
+      PRICE_CHANGES[obj.address].shift();
+      CURRENT_PRICE_TREND[obj.address] = tokenChange;
     }
   }
-  console.log(priceChanges);
+
   AnalyzePriceChanges();
-  // CashOut();
+  CashOut();
+
   monitoringPrice = false;
 }
 
 // RETURN TOP 5 GAINING COINS
 async function AnalyzePriceChanges() {
-  var tempTop5 = Object.keys(currentPriceTrend).map(function (key) {
-    return [key, currentPriceTrend[key]];
+  var tempTop5 = Object.keys(CURRENT_PRICE_TREND).map(function (key) {
+    return [key, CURRENT_PRICE_TREND[key]];
   });
   tempTop5.sort(function (first, second) {
     return second[1] - first[1];
   });
-  console.log(tempTop5.slice(0, 5));
+  console.log(tempTop5.slice(0, 10));
 }
 
 // THE SKY IS FALLING, SELL IT ALL
 async function CashOut() {
   const options = { chain: 'polygon', address: ACCOUNT };
   const balances = await Moralis.Web3API.account.getTokenBalances(options);
-  console.log("Oh no, everything is dipping, thankfully you created me master.");
+  console.log("Oh no, everything is dipping, thankfully you created me, master.");
+
   for (var i = 0; i < balances.length; i++) {
-    ConvertToStable(balances[i].token_address, balances[i].balance, balances[i].decimals);
+    if (!STABLE_COINS.includes(balances[i].token_address) && !SHIT_COINS.includes(balances[i].token_address)) {
+      SwapCoins(balances[i].token_address, STABLE_COINS[1], balances[i].balance);
+    }
   }
 }
 
 // CONVERT TOKEN TO STABLE COIN
-async function ConvertToStable(tokenIn, amountToTrade, decimals) {
+async function SwapCoins(tokenIn, tokenOut, amountToTrade) {
   // POPULATE INFO FOR SETTINGS
   const moment = require('moment')
   const now = moment().unix()
   const DEADLINE = now + 60
   const MATIC = "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270";
+  const QUICK = "0x831753dd7087cac61ab5644b308642cc1c33dc13";
   const ETH = "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619";
   const USDT = "0xc2132d05d31c914a87c6611c10748aeb04b58e8f";
-  const STABLE_COINS = [
-    "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
-    "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
-    "0xa3fa99a148fa48d14ed51d610c367c61876997f1",
-    "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063",
-    "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270"
-  ]
-  const SHIT_COINS = [
-    "0x22e51bae3f545255e115090202a23c7ede0b00b9"
-  ]
 
-  // IGNORE IF COIN IS STABLE OR SHIT (PREVENT CYCLING TRADES IN CASH OUT, ERRORS FROM SHIT COINS)
-  if (!STABLE_COINS.includes(tokenIn) && !SHIT_COINS.includes(tokenIn)) {
-    try {
-      // SET OPTIONS AND SETTINGS FOR APPROVE/SWAP
-      const OPTIONS = {
-        address: tokenIn,
-        chain: "polygon",
-        exchange: "quickswap"
-      }
-      const SETTINGS = {
-        gasLimit: 8000000,
-        gasPrice: web3.utils.toWei('50', 'Gwei'),
-        from: ACCOUNT,
-      }
+  try {
+    // SET OPTIONS AND SETTINGS FOR APPROVE/SWAP
+    const OPTIONS_IN = {
+      address: tokenIn,
+      chain: "polygon",
+      exchange: "quickswap"
+    }
+    const OPTIONS_OUT = {
+      address: tokenOut,
+      chain: "polygon",
+      exchange: "quickswap"
+    }
+    const SETTINGS = {
+      gasLimit: 8000000,
+      gasPrice: web3.utils.toWei('50', 'Gwei'),
+      from: ACCOUNT,
+    }
 
-      // GET SWAP AMOUNT BASED ON TOKEN PRICE
-      const tokenPriceForSwap = await Moralis.Web3API.token.getTokenPrice(OPTIONS);
-      const totalValue = (Moralis.Units.FromWei(amountToTrade, decimals) * (tokenPriceForSwap.usdPrice * 0.99));
-      const buyAmount = Moralis.Units.Token(totalValue.toFixed(2), "6");
-      const approveAmount = Moralis.Units.Token((totalValue).toFixed(2), "18");
+    // GET SWAP AMOUNT BASED ON TOKEN PRICE
+    const tokenInPrice = await Moralis.Web3API.token.getTokenPrice(OPTIONS_IN);
+    const tokenOutPrice = await Moralis.Web3API.token.getTokenPrice(OPTIONS_OUT);
+    const decInOptions = { chain: "polygon", addresses: tokenIn };
+    const decOutOptions = { chain: "polygon", addresses: tokenOut };
+    const tokenInMetadata = await Moralis.Web3API.token.getTokenMetadata(decInOptions);
+    const tokenOutMetadata = await Moralis.Web3API.token.getTokenMetadata(decOutOptions);
+    const tokenInDecimals = tokenInMetadata[0].decimals;
+    const tokenOutDecimals = tokenOutMetadata[0].decimals;
+    const ethIn = Moralis.Units.FromWei(amountToTrade, tokenInDecimals);
+    const cashIn = (ethIn * tokenInPrice.usdPrice * SLIPPAGE).toFixed(18);
+    const converted = cashIn / tokenOutPrice.usdPrice;
+    console.log(converted);
+    const weiOut = Moralis.Units.Token(converted, tokenOutDecimals);
+    // const weiOut = BigInt((converted * (Math.pow(10, tokenOutDecimals))).toFixed(0));
 
-      // TOKEN CONTRACT SETTINGS
-      const tokenABI = require("./erc20ABI.json");
-      const tokenAddress = tokenIn;
-      const tokenContract = new web3.eth.Contract(tokenABI, tokenAddress);
+    // TOKEN CONTRACT SETTINGS      
+    const address = tokenIn;
+    const tokenContract = new web3.eth.Contract(ERC20_ABI, address);
 
-      // CHECK FOR APPROVAL AND EXECUTE TRADE
-      const currentApproval = await tokenContract.methods.allowance(ACCOUNT, qsAddress).call();
-      console.log("\nCashing out $", totalValue, "\n");
-      if (buyAmount < currentApproval) {
-        if (currentApproval == 0) {
-          console.log("No approval, approving spend of", approveAmount);
-          executingTrade = true;
-          await tokenContract.methods.approve(qsAddress, approveAmount).send(SETTINGS);
-          let result = await qsSwapContract.methods.swapExactTokensForTokens(BigInt(amountToTrade), buyAmount, [tokenIn, MATIC, USDT], ACCOUNT, DEADLINE).send(SETTINGS)
-          console.log(`\nSuccessful Swap: https://polygonscan.com/tx/${result.transactionHash}\n`);
-          executingTrade = false;
-        }
-        else {
-          console.log("Current approval too low, increasing to", approveAmount);
-          executingTrade = true;
-          await tokenContract.methods.increaseAllowance(qsAddress, approveAmount).send(SETTINGS);
-          let result = await qsSwapContract.methods.swapExactTokensForTokens(BigInt(amountToTrade), buyAmount, [tokenIn, MATIC, USDT], ACCOUNT, DEADLINE).send(SETTINGS)
-          console.log(`\nSuccessful Swap: https://polygonscan.com/tx/${result.transactionHash}\n`);
-          executingTrade = false;
-        }
+    // CHECK FOR APPROVAL AND EXECUTE TRADE
+    const currentApproval = await tokenContract.methods.allowance(ACCOUNT, QUICKSWAP_ADDRESS).call();
+    console.log("\nCashing out $", cashIn, "\n");
+
+    if (weiOut > currentApproval) {
+      if (currentApproval == 0) {
+        console.log("\nNo approval, approving spend of", weiOut, "\n");
+        executingTrade = true;
+        await tokenContract.methods.approve(QUICKSWAP_ADDRESS, weiOut).send(SETTINGS);
+        let result = await QUICKSWAP_CONTRACT.methods.swapExactTokensForTokens(BigInt(amountToTrade), weiOut, [tokenIn, QUICK, tokenOut], ACCOUNT, DEADLINE).send(SETTINGS)
+        console.log(`\nSuccessful Swap: https://polygonscan.com/tx/${result.transactionHash}\n`);
+        executingTrade = false;
       }
       else {
-        console.log("Within approved amount, swapping")
+        console.log("\nCurrent approval too low, increasing to", weiOut, "\n");
         executingTrade = true;
-        let result = await qsSwapContract.methods.swapExactTokensForTokens(BigInt(amountToTrade), buyAmount, [tokenIn, ETH, USDT], ACCOUNT, DEADLINE).send(SETTINGS)
+        await tokenContract.methods.increaseAllowance(QUICKSWAP_ADDRESS, weiOut).send(SETTINGS);
+        let result = await QUICKSWAP_CONTRACT.methods.swapExactTokensForTokens(BigInt(amountToTrade), weiOut, [tokenIn, QUICK, tokenOut], ACCOUNT, DEADLINE).send(SETTINGS)
         console.log(`\nSuccessful Swap: https://polygonscan.com/tx/${result.transactionHash}\n`);
         executingTrade = false;
       }
     }
-
-    catch (error) {
-      console.log(error)
-      return;
+    else {
+      console.log("\nWithin approved amount, swapping\n")
+      executingTrade = true;
+      let result = await QUICKSWAP_CONTRACT.methods.swapExactTokensForTokens(BigInt(amountToTrade), weiOut, [tokenIn, QUICK, tokenOut], ACCOUNT, DEADLINE).send(SETTINGS)
+      console.log(`\nSuccessful Swap: https://polygonscan.com/tx/${result.transactionHash}\n`);
+      executingTrade = false;
     }
+  }
+
+  catch (error) {
+    console.log(error);
+    return;
   }
 }
 
-
-
-// Check markets every n seconds`
-const POLLING_INTERVAL = 1000 // 1 Second = 1000
 priceMonitor = setInterval(async () => { await monitorPrice() }, POLLING_INTERVAL)
